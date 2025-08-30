f4e609e21a30f78fa557a36df3bea00e20ed0e7c
Shitikov Kirill <shitikovkirillt@gmail.com>
2025-08-30 22:27:37 +0200

    Fix import

--- default.nix
+++ default.nix
@@ -1,45 +1,5 @@
-{ lib, config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-with lib;
-
-let
-  cfg = config.services.devServerConfig;
-  gitConfig = import ./git;
-  hConfig = import ./h;
-in {
-  options = {
-    services.devServerConfig = {
-      enable = mkOption {
-        default = false;
-        type = types.bool;
-        description = ''
-          Enable dev server.
-        '';
-      };
-      user = mkOption {
-        default = "nixos";
-        type = types.str;
-        description = "The default username for your system.";
-        example = "exampleUser";
-      };
-      codePath = mkOption {
-        default = "~/Development";
-        type = types.str;
-        description = "The default username for your system.";
-        example = "~/Development";
-      };
-    };
-  };
-
-  config = mkIf (cfg.enable) ({
-    imports = [ ./aliases.nix ];
-  } // gitConfig {
-    pkgs = pkgs;
-    user = cfg.user;
-  } // hConfig {
-    pkgs = pkgs;
-    codePath = cfg.codePath;
-  }
-
-  );
+{
+  imports = [ ./default.nix ./aliases.nix ];
 }
