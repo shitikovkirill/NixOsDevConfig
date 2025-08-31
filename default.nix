81e0c8abfafa0c2ddc7dc193f19ba314415a6da7
Shitikov Kirill <shitikovkirillt@gmail.com>
2025-08-31 07:05:29 +0200

    Refactor

--- default.nix
+++ default.nix
@@ -1,5 +1,39 @@
-{ config, lib, pkgs, ... }:
+{ lib, config, pkgs, ... }:
 
-{
-  imports = [ ./configuration.nix ./aliases.nix ];
+with lib;
+
+let
+  cfg = config.services.devServerConfig;
+  user = cfg.user;
+  codePath = cfg.codePath;
+in {
+  options = {
+    services.devServerConfig = {
+      enable = mkOption {
+        default = false;
+        type = types.bool;
+        description = ''
+          Enable dev server.
+        '';
+      };
+      user = mkOption {
+        default = "nixos";
+        type = types.str;
+        description = "The default username for your system.";
+        example = "exampleUser";
+      };
+      codePath = mkOption {
+        default = "~/Development";
+        type = types.str;
+        description = "The default username for your system.";
+        example = "~/Development";
+      };
+    };
+  };
+
+  imports = [ ./git ./h ./aliases.nix ];
+
+  config = mkIf (cfg.enable) {
+
+  };
 }
